91339cdb021da09153987d6a01126b106afe1e21
Jacob Andrean <andreanjacob99@gmail.com>
2020-10-20 14:57:02 +0700

    Initial Commit

---
+++ CustomRoundedJacobFramework/CustomRoundedJacobFramework.h
@@ -0,0 +1,18 @@
+//
+//  CustomRoundedJacobFramework.h
+//  CustomRoundedJacobFramework
+//
+//  Created by Jacob Andrean on 20/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomRoundedJacobFramework.
+FOUNDATION_EXPORT double CustomRoundedJacobFrameworkVersionNumber;
+
+//! Project version string for CustomRoundedJacobFramework.
+FOUNDATION_EXPORT const unsigned char CustomRoundedJacobFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomRoundedJacobFramework/PublicHeader.h>
+
+
